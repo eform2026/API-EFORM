a537b6a1fdd3a740514eff4189aab7d58d973371
MoGaDeX2619 <rodriguezpixel31@gmail.com>
2026-03-22 11:45:55 -0700

    api_inventario

--- api_productos/src/main/java/co/sena/edu/mogadex/api_productos/api_productos/repository/InventarioRepository.java
+++
@@ -1,7 +0,0 @@
-package co.sena.edu.mogadex.api_productos.api_productos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import co.sena.edu.mogadex.api_productos.api_productos.model.Inventario;
-
-public interface InventarioRepository extends JpaRepository<Inventario, Long> {
-}
